68a71e5ccecf9a6345a7f635b3f05d3ed8e2891d
Tom Pinataro <tompinataro@gmail.com>
2024-10-24 13:23:29 -0500

    Co-authored-by: Markv200 <Markv200@users.noreply.github.com>

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,21 +6,51 @@ const router = express.Router();
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-  res.sendStatus(200); // For testing only, can be removed
+  const queryText = `SELECT * FROM "item"`;
+  pool.query(queryText)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => {
+      console.error('Error completing SELECT shelf query', err);
+      res.sendStatus(500);
+    });
 });
+
 
 /**
  * Add an item for the logged in user to the shelf
  */
 router.post('/', (req, res) => {
-  // endpoint functionality
+  const { description, image_url } = req.body;
+  const queryText = `INSERT INTO "item" (description, image_url, user_id) VALUES ($1, $2, $3) RETURNING *`;
+  pool.query(queryText, [description, image_url, req.user.id])
+    .then(result => res.status(201).json(result.rows[0]))
+    .catch(err => {
+      console.error('Error completing INSERT shelf query', err);
+      res.sendStatus(500);
+    });
 });
+
 
 /**
  * Delete an item if it's something the logged in user added
  */
 router.delete('/:id', (req, res) => {
-  // endpoint functionality
+  const itemId = req.params.id;
+  const queryText = `DELETE FROM "item" WHERE id=$1 AND user_id=$2`;
+  pool.query(queryText, [itemId, req.user.id])
+    .then(result => {
+      if (result.rowCount > 0) {
+        res.sendStatus(204);
+      } else {
+        res.sendStatus(403); // Forbidden if the user doesn't own the item
+      }
+    })
+    .catch(err => {
+      console.error('Error completing DELETE shelf query', err);
+      res.sendStatus(500);
+    });
 });
 
 /**
